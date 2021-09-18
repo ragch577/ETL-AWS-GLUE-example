8d2a664df142a9af18b9a56954e02f0d6d47cdcd
Raghuveer Chimata <37599615+ragch577@users.noreply.github.com>
2021-09-18 23:56:35 +0200

    Add files via upload

---
+++ customer.sql
@@ -0,0 +1,13 @@
+create table customer (
+  age integer    not null,
+  annualincome     integer    not null,
+  callingnum      integer not null,
+  customerid      integer not null,
+  customersuspended integer not null,
+  education varchar(300) not null,
+  gender varchar(100) not null,
+  homeowner varchar(100) not null,
+  martialstatus varchar(100) not null,
+  occupation varchar(300) not null,
+  state    varchar(100)    not null
+)
